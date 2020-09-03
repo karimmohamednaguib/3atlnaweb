171a9db5f15c132dbf548b60f4d562015dbb9aa4
karimmohamednaguib <karim-95naguib@outlook.com>
2020-09-03 23:29:52 +0200

    3atlna web

---
+++ javascript.js
@@ -0,0 +1,23 @@
+
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+  
+  var acc = document.getElementsByClassName("btndropdown");
+  var i;
+  
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      /* this.classList.toggle("active"); */
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      } 
+    });
+  }
